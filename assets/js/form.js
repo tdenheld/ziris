675d24daebb9a655d66c89c3c6d80247297c90c1
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2019-02-10 20:59:59 +0100

    add form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,5 @@
-// form
+// store current course title in session
 // ------------------------------------------------------------
-
 if ($(".js-cta")[0]) {
     sessionStorage.setItem("course", $(".js-cta").attr("course"));
 };
@@ -8,3 +7,48 @@ if ($(".js-cta")[0]) {
 if ($(".js-signup-title")[0]) {
     $(".js-signup-title").text(sessionStorage.getItem("course"));
 };
+
+
+// form field
+// ------------------------------------------------------------
+
+function form() {
+    function formField(i) {
+        var id = "#js-formfield-" + i;
+        var input = $(id + " .js-input");
+        var label = $(id + " .js-label");
+        var tick = $(id + " .js-tick");
+
+        function checkValue() {
+            if (input.val() != "") {
+                tick.addClass("is-active");
+            } else {
+                tick.removeClass("is-active");
+            };
+        };
+        checkValue();
+
+        input.keyup(() => {
+            tick.removeClass("is-active");
+            if (input.val() != "") {
+                label.addClass("is-active");
+            } else {
+                label.removeClass("is-active");
+            };
+        });
+
+        input.focusout(function () {
+            checkValue();
+        });
+    };
+
+    if ($(".js-formfield")[0]) {
+        $(".js-formfield").each(function (i) {
+            $(this).attr("id", "js-formfield-" + i);
+            formField(i);
+        });
+    };
+};
+$(function () {
+    form();
+});
