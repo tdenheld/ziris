2e8ed67421f361efe503fd592a4ae8ab2497be69
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2019-02-10 13:50:22 +0100

    add text to inschrijven.html

--- _site/assets/js/form.js
+++ _site/assets/js/form.js
@@ -4,6 +4,7 @@
 if ($(".js-cta")[0]) {
     sessionStorage.setItem("course", $(".js-cta").attr("course"));
 };
+
 if ($(".js-signup-title")[0]) {
     $(".js-signup-title").text(sessionStorage.getItem("course"));
 };
