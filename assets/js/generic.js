980b83a722f4f0ab95556ac9cfef363641c35efc
Tibor den Held <tibor.denheld@ns.nl>
2019-11-07 21:16:38 +0100

    cleanup

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -2,66 +2,12 @@
 // ------------------------------------------------------------
 
 
-// global vars
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-var vw = Number;
-var vh = Number;
-var mobile = Boolean;
-
-
-
-// update screen height
-// ------------------------------------------------------------	
-function updateWindowSize() {
-    vw = $(window).innerWidth();
-    vh = $(window).innerHeight();
-
-    // set breakpoints
-    if (vw > 912) {
-        mobile = false;
-    } else {
-        mobile = true;
-    };
-};
-updateWindowSize();
-
-// update when resizing
-$(window).resize(function () {
-    updateWindowSize();
-});
-
-
-
-// basic toggle class
-// ------------------------------------------------------------
-$(".js-toggle").click(function () {
-    $(this).toggleClass("is-active");
-});
-
-
-
-// basic tween object constructor for smooth transitions
-// ------------------------------------------------------------
-function Transition(obj) {
-    this.obj = obj;
-    this.tween = TweenMax.fromTo(this.obj, 1, {
-        autoAlpha: 0,
-        display: "none"
-    }, {
-        ease: Power3.easeInOut,
-        autoAlpha: 1,
-        display: "block",
-    }).pause();
-};
-
-
 
 // discover
 // ------------------------------------------------------------
 function discover() {
     var obj = $(".js-discover");
-    if (obj[0] && !mobile) {
+    if (obj[0]) {
         obj.click(() => {
             TweenMax.to(window, .8, {
                 ease: Power3.easeInOut,
