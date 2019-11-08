ee814784946a21b363edc1d5b26d80e1057e4900
Tibor den Held <tibor.denheld@ns.nl>
2019-11-08 10:35:23 +0100

    update scroll reveal | not using scrollMagic

--- _site/assets/js/generic.js
+++ _site/assets/js/generic.js
@@ -1,74 +1,124 @@
 // generic javascript
 // ------------------------------------------------------------
+'use strict';
 
+const ß = (node, element) => {
+    const obj = element || document;
+    const qs = obj.querySelectorAll(node);
+    return Array.from(qs);
+}
+
+const exists = node => {
+    return document.body.contains(document.querySelector(node));
+}
+
+const isHidden = node => {
+    return window.getComputedStyle(node).display === 'none';
+}
+
+const valueInArray = (value, array) => {
+    return array.includes(value);
+}
+
+const checkLocalStorage = (value) => {
+    if (localStorage.getItem(value)) return localStorage.getItem(value).trim();
+}
+
+const removeAllChilds = node => {
+    while (node.firstChild) node.removeChild(node.firstChild);
+}
+
+const toggle = () => {
+    const obj = '.js-toggle';
+    if (!exists(obj)) return;
+    ß(obj).map((el) => el.onclick = () => el.classList.toggle('is-active'));
+}
+
+const scrollToObject = () => {
+    const obj = '.js-scroll-to';
+    const offset = document.querySelector('.header').offsetHeight + 24;
+    if (!exists(obj)) return;
+
+    ß(obj).map((el) => el.onclick = () => {
+        const target = el.getAttribute('href');
+        if (!exists(target)) return;
+        TweenMax.to(window, .8, {
+            ease: Power3.easeInOut,
+            scrollTo: {
+                y: target,
+                offsetY: offset,
+                autoKill: false,
+            }
+        });
+        return false;
+    });
+}
 
 
 // discover
 // ------------------------------------------------------------
-function discover() {
-    var obj = $(".js-discover");
-    if (obj[0]) {
-        obj.click(() => {
-            TweenMax.to(window, .8, {
-                ease: Power3.easeInOut,
-                scrollTo: {
-                    y: ".art",
-                    autoKill: false,
-                }
-            });
+const discover = () => {
+    const node = '.js-discover';
+    const obj = document.querySelector(node);
+    if (!exists(node)) return;
+
+    obj.onclick = () => {
+        TweenMax.to(window, .8, {
+            ease: Power3.easeInOut,
+            scrollTo: {
+                y: ".art",
+                autoKill: false,
+            }
         });
-        var controller = new ScrollMagic.Controller();
-        var scrll = new ScrollMagic.Scene({
-                triggerElement: window,
-                duration: 999999,
-            })
-            .triggerHook(0)
-            .offset(60)
-            .on("start", function () {
-                obj.toggleClass("o-0");
-            })
-            //.addIndicators()
-            .addTo(controller);
-    };
-};
-discover();
+    }
+
+    const reveal = () => {
+        const nodePosition = obj.getBoundingClientRect();
+        const inViewport = !(nodePosition.top > innerHeight * 0.82);
+        inViewport ? obj.classList.add('o-0') : obj.classList.remove('o-0');
+    }
+    reveal();
+
+    window.addEventListener('scroll', () => requestAnimationFrame(reveal));
+    window.addEventListener('resize', () => requestAnimationFrame(reveal));
+}
 
 
 
-// scroll magic
+// reveal on scroll
 // ------------------------------------------------------------------
-function scrollMagic() {
-    var section = $(".js-scroll-magic");
+const revealOnScroll = () => {
+    const section = '.js-scroll';
+    const richTxt = '.js-scroll-rt > *';
+    if (!exists(section) && !exists(richTxt)) return;
 
-    if (section[0]) {
-        var controller = new ScrollMagic.Controller({
-            globalSceneOptions: {
-                duration: 99999,
-            },
+    const init = (node) => {
+        const reveal = () => ß(node).map((el) => {
+            const defaultHook = 0.92;
+            const hook = el.getAttribute('data-hook') || defaultHook;
+
+            const nodePosition = el.getBoundingClientRect();
+            const inViewport = !(nodePosition.top > innerHeight * hook);
+
+            if (inViewport) {
+                ß('.js-tr', el).map((ae) => ae.classList.add('is-active'));
+                if (el.classList.contains('js-tr')) el.classList.add('is-active');
+            }
         });
+        reveal();
 
-        function scrollTrig(obj) {
-            var scrll = new ScrollMagic.Scene({
-                    triggerElement: obj,
-                })
-                .triggerHook(0.94)
-                .offset(0)
-                .on("start", function () {
-                    $('.js-tr', obj).toggleClass('is-active');
-                    if ($(obj).hasClass("js-tr")) {
-                        $(obj).toggleClass("is-active");
-                    };
-                })
-                //.addIndicators()
-                .addTo(controller);
-        };
+        window.addEventListener('scroll', () => requestAnimationFrame(reveal));
+        window.addEventListener('resize', () => requestAnimationFrame(reveal));
+    }
 
-        section.each(function () {
-            scrollTrig(this);
-        });
-    };
-};
+    ß(richTxt).map((el) => el.classList.add('js-tr', 'tr-fi-up', 'tr-1500'));
+    init(richTxt);
+    init(section);
+}
 
-$(function () {
-    scrollMagic();
+document.addEventListener('DOMContentLoaded', () => {
+    toggle();
+    scrollToObject();
+    discover();
+    revealOnScroll();
 });
